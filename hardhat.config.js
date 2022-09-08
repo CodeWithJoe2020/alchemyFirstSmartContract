d0260fa4b431e989344e12967db5e100e044d5f6
CodeWithJoe2020 <75099121+CodeWithJoe2020@users.noreply.github.com>
2022-09-08 17:54:50 +0200

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,20 +1,25 @@
-/**
-* @type import('hardhat/config').HardhatUserConfig
-*/
+// hardhat.config.js
 
 require('dotenv').config();
 require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-etherscan");
 
 const { API_URL, PRIVATE_KEY } = process.env;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 
 module.exports = {
-   solidity: "0.7.3",
-   defaultNetwork: "goerli",
-   networks: {
+  solidity: "0.7.3",
+  defaultNetwork: "goerli",
+  networks: {
       hardhat: {},
       goerli: {
          url: API_URL,
          accounts: [`0x${PRIVATE_KEY}`]
       }
-   },
-}
+  },
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: ETHERSCAN_API_KEY
+  }
+};
