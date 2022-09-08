380aa3af2b223999c8649ccf8e7da16bf0f022c8
CodeWithJoe2020 <75099121+CodeWithJoe2020@users.noreply.github.com>
2022-09-08 23:48:05 +0200

    Update react-controllfow.js

--- react-controllfow.js
+++ react-controllfow.js
@@ -1,4 +1,18 @@
 interact.js
+require('dotenv').config();
+const alchemyKey = process.env.REACT_APP_ALCHEMY_KEY;
+const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
+const web3 = createAlchemyWeb3(alchemyKey); 
+
+const contractABI = require('../contract-abi.json')
+const contractAddress = "0xBB17CCc17a997CB880807910A2A32D12879a8D35";
+
+export const helloWorldContract = new web3.eth.Contract(
+  contractABI,
+  contractAddress
+);
+
+
 export const loadCurrentMessage = async () => { 
     const message = await helloWorldContract.methods.message().call(); 
     return message;
